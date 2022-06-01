3093b846afd3d0342645d3362c33dc2b8884b01e
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-06-01 20:38:12 +0430

    log add account

--- pages/api/addAccount.js
+++ pages/api/addAccount.js
@@ -15,12 +15,14 @@ async function handler(req, res) {
   await runMiddleware(req, res)
 
   const { email, password, } = req.body;
+  console.log(email, password)
   const result = await fetch("https://api.nobitex.ir/auth/login/", {
     body: JSON.stringify({ username: email, password: password, captcha: "api", remember: "yes" }),
     headers: { "Content-Type": "application/json" },
     method: "POST"
   })
   const data = await result.json();
+  console.log(data)
 
   if (result.status == 200) {
     return res.status(200).json({
@@ -29,7 +31,6 @@ async function handler(req, res) {
         token: data.key,
         device: data.device
       }
-
     })
   }
   return res.status(401).json({
